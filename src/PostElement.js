ab33459130c2e17656891d3d07e4709e44fbb004
Sesdika <sesdika.sansani@gmail.com>
2023-05-04 22:40:16 +0700

    change the save icon when user click it

--- src/PostElement.js
+++ src/PostElement.js
@@ -2,13 +2,16 @@ import React, {useState} from "react";
 import VideoPlayer from "./VideoPlayer";
 import comment_icon from "./photos/bubble-chat.png"
 import send_icon from "./photos/send.png"
-import save_icon from "./photos/flag.png"
+import save_icon from "./photos/save.png"
+import saved_icon from "./photos/saved.png"
+import {Link} from "react-router-dom";
 
 import TimeAgo from 'timeago-react';
-import ModalInFunctionalComponent from "./Modal";
+import ShareModal from "./ShareModal";
 
 function PostElement({mediaType='photo',media,profilePicture,username,time,likeCount=0,explanation="",user_has_liked=false,n_comments=0,comments=[]}) {
     const [isLiked, setIsLiked] = useState(user_has_liked);
+    const [isSaved, setIsSaved] = useState(false);
 
     return (
         <div>
@@ -30,7 +33,7 @@ function PostElement({mediaType='photo',media,profilePicture,username,time,likeC
                     <img className="image_viewer" src={media} alt="post"/>
                 }
                 <div className="post-element-bottom-bar">
-                    <div onClick={() => {
+                    <Link onClick={() => {
                             if (isLiked === false) return setIsLiked(true)
                             else if (isLiked === true) return setIsLiked(false)
                         }
@@ -47,13 +50,20 @@ function PostElement({mediaType='photo',media,profilePicture,username,time,likeC
                                         d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path>
                                 </svg>
                             }
-                        </div>
-                        <div><img src={comment_icon} alt="comments" width="24px"/></div>
-                        <div><img src={send_icon} alt="comments" width="24px"/></div>
-                        <ModalInFunctionalComponent/>
-                        <div><img src={save_icon} alt="comments" style={{position: "absolute", right: "0"}} height="26px"/>
-                    </div>
-
+                    </Link>
+                    <div><img src={comment_icon} alt="comments" width="24px"/></div>
+                    <ShareModal/>
+                    <Link onClick={() => {
+                            if (isSaved === false) return setIsSaved(true)
+                            else if (isSaved === true) return setIsSaved(false)
+                        }
+                        }>
+                            {isSaved ?
+                                <img src={saved_icon} alt="comments" style={{position: "absolute", right: "0"}} height="26px"/>
+                                :
+                                <img src={save_icon} alt="comments" style={{position: "absolute", right: "0"}} height="26px"/>
+                            }
+                    </Link>
                 </div>
                 <div><h4 style={{margin: 0}}>{likeCount}&nbsp;likes</h4></div>
                 <div style={{display: "flex", flexDirection: "row"}}><h4
