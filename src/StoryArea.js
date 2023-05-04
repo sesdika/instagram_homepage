96fc6e27c20b5d9cf11d1110683be04f024744a4
Sesdika <sesdika.sansani@gmail.com>
2023-05-05 01:16:38 +0700

    display story from api json

--- src/StoryArea.js
+++ src/StoryArea.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
-import React, {useRef} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import right from "./photos/right.png"
 import left from "./photos/left.png"
 import test_pp_icon from "./photos/1.jpg"
 import StoryElement from "./StoryElement";
 
+import axios from "axios"
 
 function StoryArea() {
     const scrollRef = useRef();
@@ -24,36 +25,26 @@ function StoryArea() {
         });
     };
 
+    const [datastory, setDatastory] = useState([])
+    useEffect(() => {
+        axios.get("https://private-4838f9-sesdika.apiary-mock.com/stories")  
+            .then(response => setDatastory(response.data.record.data))
+            .catch(err => console.log(err)) 
+    }, []);
+
     return (
         <div>
             <div>
                 <div className="story-left-button" onClick={scrollLeft}><img src={left} alt="left"/></div>
                 <div className="story-area-box" ref={scrollRef}>
-                    <StoryElement profilePicture={test_pp_icon}>
-                        yavuzyigitmuhammetali
-                    </StoryElement>
-                    <StoryElement profilePicture={test_pp_icon}>
-                        yavuzyigitmuhammetali
-                    </StoryElement>
-                    <StoryElement profilePicture={test_pp_icon}>
-                        yavuzyigitmuhammetali
-                    </StoryElement>
-                    <StoryElement profilePicture={test_pp_icon}>
-                        yavuzyigitmuhammetali
-                    </StoryElement>
-                    <StoryElement profilePicture={test_pp_icon}>
-                        yavuzyigitmuhammetali
-                    </StoryElement>
-                    <StoryElement profilePicture={test_pp_icon}>
-                        yavuzyigitmuhammetali
-                    </StoryElement>
-                    <StoryElement profilePicture={test_pp_icon}>
-                        yavuzyigitmuhammetali
-                    </StoryElement>
-                    <StoryElement profilePicture={test_pp_icon} checkOpen={true}>
-                        yavuzyigitmuhammetali
-                    </StoryElement>
-
+                    {datastory.length?
+                        datastory.map(story=>
+                            <StoryElement profilePicture={story.from.profile_picture}>
+                                {story.from.username}
+                            </StoryElement>
+                        ) :
+                        <div></div>
+                    }
                 </div>
                 <div className="story-right-button" onClick={scrollRight}><img src={right} alt="right"/></div>
             </div>
